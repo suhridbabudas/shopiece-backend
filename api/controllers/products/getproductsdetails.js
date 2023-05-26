0703bc511f556a9038979f4d53c7ba09099ce241
Suhrid Babu Das <suhrid@Suhrids-MacBook-Pro.local>
2023-05-27 01:39:44 +0530

    new changes

--- api/controllers/products/getproductsdetails.js
+++ api/controllers/products/getproductsdetails.js
@@ -1,63 +1,43 @@
 module.exports = {
-
-
   friendlyName: 'Getproductsdetails',
-
-
   description: 'Getproductsdetails products.',
-
-
   inputs: {
-
   },
-
-
   exits: {
+    success: {
+      statusCode: 200,
+    },
     error: {
       statusCode: 400,
     }
   },
-
-
   fn: async function (exits) {
     const productID = this.req.params.id;
     try {
       const product = await Products.findOne({ id: productID });
       if (!product) {
-        return exits.error({
-          status: 200,
-          product: {}
-        })
+        return this.res.status(200).json({});
       }
       const productSubType = await ProductsSubType.findOne({ id: product.ProductsSubTypeKey });
       if (!productSubType) {
         product['ProductsType'] = null;
         product['ProductsSubType'] = null;
         product['Brands'] = null;
-        return exits.error({
-          status: 200,
-          product
-        })
+        return this.res.status(200).json(product);
       }
       const productType = await ProductsType.findOne({ TypeCode: productSubType.TypeCode });
       if (!productType) {
         product['ProductsType'] = null;
         product['ProductsSubType'] = productSubType;
         product['Brands'] = null;
-        return exits.error({
-          status: 200,
-          product
-        })
+        return this.res.status(200).json(product);
       }
       const brand = await Brands.findOne({ id: product.BrandKey });
       if (!brand) {
         product['ProductsType'] = productType;
         product['ProductsSubType'] = productSubType;
         product['Brands'] = null;
-        return exits.error({
-          status: 200,
-          product
-        })
+        return this.res.status(200).json(product);
       }
       product['ProductsType'] = productType;
       product['ProductsSubType'] = productSubType;
